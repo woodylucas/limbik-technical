f625a32c768756c6ef4833c729899199c1b75f6e
woodylucas <34947061+woodylucas@users.noreply.github.com>
2019-08-15 00:12:21 -0400

    added a single page for one specific data

--- src/components/text/Texts.js
+++ src/components/text/Texts.js
@@ -1,27 +1,56 @@
 import React from 'react'
 import axios from 'axios'
+import Spinner from '../layout/Spinner'
+import { Link } from 'react-router-dom'
 
 class Texts extends React.Component {
   state = {
-    texts: {}
+    text: {}
   }
 
   componentDidMount() {
     // this.props.match.params.id gets the id from the url we created.
     axios.get(`http://localhost:3000/data/${this.props.match.params.id}`)
       .then(resp => {
-        console.log(resp.data)
-        // this.setState({data_list: resp.data })
+        // console.log(resp.data)
+        this.setState({text: resp.data })
       })
   }
 
 
   render() {
-    return(
-      <div>
-        <h1>Texts</h1>
-      </div>
-    )
+    const { text } = this.state
+    console.log("state:", text);
+    if (
+      text === undefined ||
+      Object.keys(text).length === 0) {
+      return <Spinner />
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">Go Back</Link>
+            <div className="card">
+              <h5 className="card-header">
+
+                Text URL: <span className="text-secondary">{text.url}</span>
+              </h5>
+              <div className="card-body">
+                <p className="card-text">{text.text}</p>
+              </div>
+            </div>
+
+            <ul className="list-group mt-3">
+              <li className="list-group-item">
+                <strong>PDF: </strong>: {text.pdf}
+              </li>
+              <li className="list-group-item">
+                <strong>Created: </strong>: {text.created}
+              </li>
+            </ul>
+
+        </React.Fragment>
+      )
+    }
   }
 }
 
